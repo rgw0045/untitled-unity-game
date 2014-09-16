6d6959d7ccf33c117786c07efef4b7e834aa542d
NicholasChard12 <NicholasChard12@gmail.com>
2014-09-16 17:39:01 +0000

    Added to do list

--- Greetings.cs
+++ Greetings.cs
@@ -4,5 +4,11 @@ public class Hello1
    public static void Main()
    {
       System.Console.WriteLine("Greetings, World!");
+      
+     //to do list:
+      
+      //delete system32
+      
+      //install candy crush
    }
 }
