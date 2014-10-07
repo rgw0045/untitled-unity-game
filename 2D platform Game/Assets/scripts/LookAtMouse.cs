1d8beb44ee8ff51e09480318e7a0974584d98b60
Joham MAtos <danteapk@gmail.com>
2014-10-07 12:50:30 -0500

    arm now works

--- 2D platform Game/Assets/scripts/LookAtMouse.cs
+++ 2D platform Game/Assets/scripts/LookAtMouse.cs
@@ -3,18 +3,14 @@ using System.Collections;
 
 public class LookAtMouse : MonoBehaviour {
 
-	public float cameraDif = 10.0f;
-	//Vector3 pos;
+	public float rotationOffset = 10.0f;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
 	// Update is called once per frame
 	void Update () {
-		Vector3 pos = Vector3(Input.mousePosition.x, Input.mousePosition.y, cameraDif);
-		pos = Camera.main.ScreenToWorldPoint(pos);
-		transform.position = pos;
+		Vector3 difference = Camera.main.ScreenToWorldPoint (Input.mousePosition) - transform.position;
+		difference.Normalize ();
+
+		float rotateZ = Mathf.Atan2 (difference.y, difference.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Euler (0f, 0f, rotateZ * rotationOffset);
 	}
 }
