28d16b66b41df40c1cea81ad69984f6638f2f91d
Ricky <ricky@dynamic-10-124-21-104.dynamic.unt.edu>
2014-10-23 11:57:22 -0500

    sasf

---
+++ Unititled-Unity-Game/Assets/scripts/MovingPlatform.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatform : MonoBehaviour {
+
+	public Transform Destination;
+	public Transform Origin;
+	public float speed;
+	public bool Switch = false;
+
+	void FixedUpdate()
+	{
+		if (transform.position == Destination.position) 
+		{
+			Switch = true;
+		}
+
+		if (transform.position == Origin.position) 
+		{
+			Switch = false;
+		}
+
+		if (Switch) 
+		{
+			transform.position = Vector3.MoveTowards (transform.position, Origin.position, speed);
+		} 
+		else 
+		{
+			transform.position = Vector3.MoveTowards(transform.position, Destination.position, speed);
+		}
+
+
+	}
+
+}
