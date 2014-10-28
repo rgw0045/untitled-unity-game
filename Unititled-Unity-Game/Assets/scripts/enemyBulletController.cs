398b8b64723bd2b559307ce3b7cc611c9cfbdbb4
Gary Johnson <garyleejohnsonjr@gmail.com>
2014-10-27 20:42:22 -0500

    Enemy shoots at player when in range - Gary

---
+++ Unititled-Unity-Game/Assets/scripts/enemyBulletController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyBulletController : MonoBehaviour {
+	public float speed;
+	public float range;
+
+	Vector3 startPosition;
+	Vector2 vector;
+	float distance;
+
+	Vector2 target;
+	
+	// Use this for initialization
+	void Start () {
+		startPosition = transform.position;
+		
+		target = GameObject.Find("Player").transform.position;
+
+		Vector2 difference = target - (Vector2)transform.position;
+
+		rigidbody2D.velocity = difference.normalized * speed;
+
+	}
+	
+	void Update() {
+		vector = transform.position - startPosition;
+		distance = vector.magnitude;
+		if (distance >= range) Destroy(this.gameObject);
+	}
+}
