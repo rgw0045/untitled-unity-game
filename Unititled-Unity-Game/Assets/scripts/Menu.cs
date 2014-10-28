58787aeeb5879f8788f5b4587e1686a0bceacbf0
Joham MAtos <danteapk@gmail.com>
2014-10-28 11:28:24 -0500

    the menu script

---
+++ Unititled-Unity-Game/Assets/scripts/Menu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	public Texture backgroundTexture;
+
+	//Display Background
+	void OnGUI()
+	{
+		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), backgroundTexture);
+
+
+	//GUI button
+
+		if(GUI.Button (new Rect(Screen.width * .25f, Screen.height *.35f, Screen.width *.5f, Screen.height *.1f), "Play"))
+		{
+			Application.LoadLevel(1);
+			//debug
+			print("Clicked Play");
+		}
+		if(GUI.Button (new Rect(Screen.width * .25f, Screen.height *.75f, Screen.width *.5f, Screen.height *.1f), "Exit"))
+		{
+			Application.Quit();
+			//debug
+			print("Clicked Exit");
+		}
+	}
+}
+		                    
