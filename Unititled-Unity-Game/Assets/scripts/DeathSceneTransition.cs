93157497dbdf9b49c56baf8cdf7758e166485bad
Ricky <ricky@dynamic-10-124-21-50.dynamic.unt.edu>
2014-10-27 21:48:29 -0500

    Added Exit door and Camera in Scene 2

---
+++ Unititled-Unity-Game/Assets/scripts/DeathSceneTransition.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathSceneTransition : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other){
+
+		if(other.tag == "Player")
+			Application.LoadLevel("DeathScene");
+	}
+	
+
+}
