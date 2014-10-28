0c821277bd108cfcd780a9355b3e47a4aee6d2da
Joham MAtos <danteapk@gmail.com>
2014-10-28 11:26:34 -0500

    with title scene

--- Unititled-Unity-Game/Assets/scripts/RestartLevel.cs
+++ Unititled-Unity-Game/Assets/scripts/RestartLevel.cs
@@ -7,7 +7,7 @@ public class RestartLevel : MonoBehaviour {
 		GUI.Box (new Rect (Screen.width / 2, Screen.height / 2, 100, 90), "Restart");
 
 		if(GUI.Button (new Rect((Screen.width/2)+10, (Screen.height/2)+30, 80, 20), "First Level"))
-		               Application.LoadLevel(0);
+		               Application.LoadLevel(1);
 		if(GUI.Button (new Rect((Screen.width/2)+10, (Screen.height/2)+60, 80, 20), "Exit"))
 			Application.Quit();
 	}
