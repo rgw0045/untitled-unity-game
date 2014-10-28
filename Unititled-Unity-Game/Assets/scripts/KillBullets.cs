251a5521c204501ffcc5d65797235100c7dda181
Ricky <ricky@dynamic-10-124-21-50.dynamic.unt.edu>
2014-10-27 19:34:14 -0500

    Platforms Stop bullets and added detail

---
+++ Unititled-Unity-Game/Assets/scripts/KillBullets.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillBullets : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other){
+
+		if (other.tag == "Bullet")
+		  Destroy (other.gameObject);
+	}
+
+	
+}
