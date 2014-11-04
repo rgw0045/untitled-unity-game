517e3544ad4d2df31b45b44e01310c9c69b8e7f2
Ricky <ricky@dynamic-10-124-21-187.dynamic.unt.edu>
2014-11-04 11:27:23 -0600

    jeqnjq

---
+++ Unititled-Unity-Game/Assets/scripts/TestCamera.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestCamera : MonoBehaviour {
+
+	public float dragSpeed = 2.0f;
+	private Vector3 dragOrigin;
+	private bool isPanning;
+	
+	
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetMouseButtonDown(0)) 
+		{
+			dragOrigin = Input.mousePosition;
+			isPanning = true;
+		}
+		
+		if (!Input.GetMouseButton(0)) 
+		{
+			isPanning = false;
+		}
+		
+		if (isPanning) 
+		{
+			Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+			
+			Vector3 move = new Vector3(pos.x * dragSpeed, pos.y * dragSpeed, 0);
+			transform.Translate(move, Space.Self);
+			
+			transform.position = new Vector3(Mathf.Clamp(transform.position.x, -10,10),Mathf.Clamp(transform.position.y, -10,10),0);
+		}
+	}
+
+
+}
