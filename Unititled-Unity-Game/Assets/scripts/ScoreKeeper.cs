992dacfc2a66fa3194627c1f905c20f5747b2921
Ricky <ricky@dynamic-10-124-21-158.dynamic.unt.edu>
2014-11-13 12:58:56 -0600

    Script to keep score has been added

---
+++ Unititled-Unity-Game/Assets/scripts/ScoreKeeper.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreKeeper : MonoBehaviour {
+
+	public GameObject player;
+	public GUIText scoreText;
+	private int score;
+
+	void Start(){
+
+		DontDestroyOnLoad (player);
+		score = 0;
+		UpdateScore ();
+	}
+
+	public void Addscore(int modifier){
+
+		score += modifier;
+		UpdateScore ();
+	}
+
+	void UpdateScore(){
+
+		scoreText.text = "Score: " + score;
+	}
+
+}
