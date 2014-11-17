b822991e319e6f6224d35c9cd91de3a2dcd36082
Joham MAtos <danteapk@gmail.com>
2014-11-17 14:51:03 -0600

    a better way to fill the health back to max on the death scene

--- Unititled-Unity-Game/Assets/scripts/RestartLevel.cs
+++ Unititled-Unity-Game/Assets/scripts/RestartLevel.cs
@@ -6,8 +6,10 @@ public class RestartLevel : MonoBehaviour {
    void OnGUI() {
 		GUI.Box (new Rect (Screen.width / 2, Screen.height / 2, 100, 90), "Restart");
 
-		if(GUI.Button (new Rect((Screen.width/2)+10, (Screen.height/2)+30, 80, 20), "First Level"))
-		               Application.LoadLevel(1);
+		if (GUI.Button (new Rect ((Screen.width / 2) + 10, (Screen.height / 2) + 30, 80, 20), "First Level")) {
+			HealthBar.curHealth = HealthBar.maxHealth;
+			Application.LoadLevel (1);
+		}
 		if(GUI.Button (new Rect((Screen.width/2)+10, (Screen.height/2)+60, 80, 20), "Exit"))
 			Application.Quit();
 	}
