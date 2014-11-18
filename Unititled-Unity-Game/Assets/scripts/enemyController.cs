be7eb7e7e80d5ae5fb9f3ad812df5e4bf6ecec02
Gary Johnson <garyleejohnsonjr@gmail.com>
2014-11-18 12:36:34 -0600

    Updated enemies to patrol. - Gary

--- Unititled-Unity-Game/Assets/scripts/enemyController.cs
+++ Unititled-Unity-Game/Assets/scripts/enemyController.cs
@@ -2,40 +2,31 @@
 using System.Collections;
 
 public class enemyController : MonoBehaviour {
-	//variables for moving
-	public float maxSpeed = 10f;
-	bool facingRight = true;
+	// public variables
+	public float maxSpeed = 1.0f;
+	public float fireRate = 0.1f;
+	public float groundRadius = 0.2f;
 	public float maxHealth = 100;
-	float curHealth;
-
 	public GameObject arm;
-	float playerX;
-	float playerY;
-	float playerZ;
-	Vector3 Player;
-
 	public GameObject shot;
 	public Transform shotSpawn;
-	public float fireRate;
-	private float nextFire;
+	public Transform groundCheck;
+	public Transform forwardGroundCheck;
+	public Transform forwardWallCheck;
+	public LayerMask whatIsGround;
 
+	// global variables
+	bool facingRight = true;
+	bool movingRight = true;
+	public bool grounded = false;
+	public bool forwardGrounded = false;
+	public bool forwardWalled = false;
+	float curHealth;
+	Vector3 playerPos;
 	Animator anim;
 
-	//variables to fall
-	bool grounded = false;
-	public Transform groundCheck;
-	float groundRadius = 0.2f;
-	public LayerMask whatIsGround;
-	public float jumpForce = 700f;
-
-	//score related things
-//	private int scoreValue = 50;
-//	private GameController scorekeeper;
-
-	bool doubleJump = false;
-
 	void Start() {
-		//gets the animator
+		// gets this object's animator
 		anim = GetComponent <Animator> ();
 		anim.SetBool ("FacingRight", facingRight);
 		curHealth = maxHealth;
@@ -43,65 +34,45 @@ public class enemyController : MonoBehaviour {
 	}
 
 	void FixedUpdate() {
-		//check to see if i am on the ground
+		// check to see if i am on the ground
 		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
 		anim.SetBool ("Ground", grounded);
 
-		if (grounded)
-			doubleJump = false;
+		// check to see if my forward ground check is on the ground
+		forwardGrounded = Physics2D.OverlapCircle (forwardGroundCheck.position, groundRadius, whatIsGround);
 
-		//jumping animation
-		anim.SetFloat ("vSpeed", rigidbody2D.velocity.y);
+		// check to see if my forward wall check is against a wall
+		forwardWalled =  Physics2D.OverlapCircle (forwardWallCheck.position, groundRadius, whatIsGround);
+		
+		// decide which direction to move
+		if (grounded && (!forwardGrounded || forwardWalled)) movingRight = !movingRight;
 
-		//gets input to move the character
-		float move = 0;
-
-		//gets the speed for the animator
-		anim.SetFloat ("speed", move);
-
-		//moves charactor
-		rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
-
-		playerX = GameObject.Find("Player").transform.position.x - transform.position.x;
-
+		// move the character
+		float xVelocity;
+		if (grounded) {
+			if (movingRight) xVelocity = maxSpeed;
+			else xVelocity = -maxSpeed;
+		}
+		else xVelocity = 0.0f;
+		rigidbody2D.velocity = new Vector2 (xVelocity, rigidbody2D.velocity.y);
+		anim.SetFloat ("speed", xVelocity);
+		
 		//if moving left and not facing right, flip
-		if (playerX > 0 && !facingRight) {
-			Flip ();
-		}
-		//if moving left and not facing left, flip
-		else if (playerX < 0 && facingRight) {
-			Flip ();
-		}
+		if ((rigidbody2D.velocity.x > 0 && !facingRight) || (rigidbody2D.velocity.x < 0 && facingRight)) Flip();
 	}
 
 	void Update() {
-		if (curHealth <= 0) {
-			Destroy (this.gameObject);
-//			ScoreKeeper.Addscore(scoreValue);
-		}
+		if (curHealth <= 0) Destroy (this.gameObject);
 	}
-
 	
 	//function to flip the walking animation
 	void Flip() {
 		facingRight = !facingRight;
 		anim.SetBool ("FacingRight", facingRight);
-		if (!facingRight) {
-			Quaternion theScale = transform.localRotation;
-			theScale.y = 180;
-			transform.localRotation = theScale;
-		}
-		else {
-			Quaternion theScale = transform.localRotation;
-			theScale.y = 0;
-			transform.localRotation = theScale;
-		}
-		Vector3 theArmScale = arm.transform.localScale;
-		//theScale.x *= -1;
-		theArmScale.y *= -1;
-		arm.transform.localScale = theArmScale;
+		transform.localScale = new Vector2 (-transform.localScale.x, transform.localScale.y);
 	}
+	
 	public void AdjustcurHealth(int adj) {
-				curHealth += adj;
-		}
+		curHealth += adj;
+	}
 }
