fa90ae18fe7f87e33c597479a0c22e7842336565
Ricky <ricky@dynamic-10-124-21-95.dynamic.unt.edu>
2014-11-20 13:56:18 -0600

    Time shows and score text is there but doesn't update yet

--- Unititled-Unity-Game/Assets/scripts/GameController.cs
+++ Unititled-Unity-Game/Assets/scripts/GameController.cs
@@ -5,13 +5,24 @@ public class GameController : MonoBehaviour {
 
 	public GameObject player;
 	public GUIText scoreText;
-	private int score;
+	public GUIText timeText;
+	private static int score;
+	private float startTime;
+	private float stageTime;
 	
 	void Start(){
 		
-		//DontDestroyOnLoad (player);
+		//DontDestroyOnLoad (GameController);
 		score = 0;
+		startTime = Time.time;
 		UpdateScore ();
+
+	}
+
+	void Update(){
+		stageTime = Time.time - startTime;
+		timeText.text = "Time: " + stageTime;
+	
 	}
 	
 	public void Addscore(int modifier){
@@ -21,7 +32,32 @@ public class GameController : MonoBehaviour {
 	}
 	
 	void UpdateScore(){
-		
 		scoreText.text = "Score: " + score;
 	}
+
+	public void TimeScore(){
+		int reward;
+
+		if (stageTime <= 20) {
+			reward = 70;
+			Addscore(reward);
+		}
+
+		if (stageTime <= 30 && stageTime > 20) {
+			reward = 50;
+			Addscore(reward);
+		}
+
+		if (stageTime <= 50 && stageTime > 30) {
+			reward = 20;
+			Addscore(reward);
+		}
+
+		if (stageTime > 50) {
+			reward = 10;
+			Addscore(reward);
+		}
+
+	}
+
 }
--- Unititled-Unity-Game/Assets/scripts/NextLevel.cs
+++ Unititled-Unity-Game/Assets/scripts/NextLevel.cs
@@ -4,9 +4,25 @@ using System.Collections;
 public class NextLevel : MonoBehaviour {
 
 	private int levelNum;
+	private GameController gameController;
+
+	void Start()
+	{
+		GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+		if (gameControllerObject != null) 
+		{
+			gameController = gameControllerObject.GetComponent<GameController>();		
+		}
+		
+		if (gameController == null) 
+		{
+			Debug.Log("Cannot Find 'GameController' Script!");		
+		}
+	}
 
 	void OnTriggerEnter2D(Collider2D other){
 				if (other.tag == "Player") {
+						gameController.TimeScore();
 						int.TryParse (Application.loadedLevelName, out levelNum);
 						levelNum += 1;
 						Application.LoadLevel (levelNum);
