664e6424e7608405f78374d52c2b591ad1c860a5
Gary Johnson <garyleejohnsonjr@gmail.com>
2014-11-20 14:02:04 -0600

    updated enemy AI - Gary

--- Unititled-Unity-Game/Assets/scripts/enemyController.cs
+++ Unititled-Unity-Game/Assets/scripts/enemyController.cs
@@ -2,77 +2,85 @@
 using System.Collections;
 
 public class enemyController : MonoBehaviour {
-	// public variables
-	public float maxSpeed = 1.0f;
-	public float fireRate = 0.1f;
-	public float groundRadius = 0.2f;
-	public float maxHealth = 100;
-	public GameObject arm;
-	public GameObject shot;
-	public Transform shotSpawn;
-	public Transform groundCheck;
-	public Transform forwardGroundCheck;
-	public Transform forwardWallCheck;
-	public LayerMask whatIsGround;
+	Animator animator;
+	LayerMask whatIsGround;
+	Object enemyBullet;
+	Transform enemy;
+    Transform player;
+	Transform weaponArm;
+	Transform shotSpawn;
+	Transform groundCheck;
+	Transform forwardGroundCheck;
+	Transform forwardWallCheck;
 
-	// global variables
-	bool facingRight = true;
-	bool movingRight = true;
-	public bool grounded = false;
-	public bool forwardGrounded = false;
-	public bool forwardWalled = false;
+	float attackRange = 8.0f;
+	float fireRate = 0.1f;
+	float nextFire = 0.0f;
+	float groundRadius = 0.2f;
+	float maxSpeed = 1.0f;
+	
+	void Start () {
+		animator = GetComponent <Animator> ();
+		player = GameObject.Find("Player").transform;
+		enemy = transform;
+		weaponArm = enemy.Find("WeaponArm");
+		shotSpawn = weaponArm.Find("ShotSpawn");
+		whatIsGround = LayerMask.GetMask("Walls") + 1;
+		groundCheck = enemy.Find("GroundCheck");
+		forwardGroundCheck = enemy.Find("ForwardGroundCheck");
+		forwardWallCheck = enemy.Find("ForwardWallCheck");
+		enemyBullet = Resources.Load("EnemyBullet");
+	}
+	
+	void FixedUpdate () {
+		// get the player's position relative to this enemy's position
+		Vector2 playerRelPos = Vector2.Scale(player.position - enemy.position, enemy.localScale);
+		
+		// if the player is in front and is in range attack the player
+		if (playerRelPos.x > 0.0f && playerRelPos.magnitude < attackRange) attack(playerRelPos);
+		else patrol();
+		
+		// set animation states
+		animator.SetFloat ("speed", rigidbody2D.velocity.x * enemy.localScale.x);
+	}
+	
+	void attack(Vector2 playerRelPos) {
+		// stop moving
+		rigidbody2D.velocity = new Vector2 (0.0f, rigidbody2D.velocity.y);
+		
+		// get the relative and absolute angle of the player
+		float playerRelAng = 90.0f - Vector2.Angle(Vector2.up, playerRelPos);
+		
+		// point gun at player
+		weaponArm.rotation = Quaternion.Euler (0.0f, 0.0f, playerRelAng);
+		
+		// shoot if enough time has passed
+		if (Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			Quaternion shotRot;
+			if (enemy.localScale.x > 0.0f) shotRot = Quaternion.Euler (0.0f, 0.0f, playerRelAng);
+			else shotRot = Quaternion.Euler (0.0f, 0.0f, 180.0f - playerRelAng);
+			Instantiate(enemyBullet, shotSpawn.position, shotRot);
+	    }
+	}
+	
+	void patrol() {
+		// point gun forward
+		weaponArm.rotation = Quaternion.Euler (0.0f, 0.0f, 0.0f);
+		
+		// flip if about to walk off platform or against a wall
+		bool grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+		bool forwardGrounded = Physics2D.OverlapCircle (forwardGroundCheck.position, groundRadius, whatIsGround);
+		bool forwardWalled =  Physics2D.OverlapCircle (forwardWallCheck.position, groundRadius, whatIsGround);
+		if (grounded && (!forwardGrounded || forwardWalled)) enemy.localScale = new Vector2(enemy.localScale.x * -1, enemy.localScale.y);
+		
+		// walk forward
+		rigidbody2D.velocity = new Vector2 (maxSpeed * enemy.localScale.x, rigidbody2D.velocity.y);
+	}
+
 	float curHealth;
-	Vector3 playerPos;
-	Animator anim;
-
-	void Start() {
-		// gets this object's animator
-		anim = GetComponent <Animator> ();
-		anim.SetBool ("FacingRight", facingRight);
-		curHealth = maxHealth;
-
-	}
-
-	void FixedUpdate() {
-		// check to see if i am on the ground
-		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
-		anim.SetBool ("Ground", grounded);
-
-		// check to see if my forward ground check is on the ground
-		forwardGrounded = Physics2D.OverlapCircle (forwardGroundCheck.position, groundRadius, whatIsGround);
-
-		// check to see if my forward wall check is against a wall
-		forwardWalled =  Physics2D.OverlapCircle (forwardWallCheck.position, groundRadius, whatIsGround);
-		
-		// decide which direction to move
-		if (grounded && (!forwardGrounded || forwardWalled)) movingRight = !movingRight;
-
-		// move the character
-		float xVelocity;
-		if (grounded) {
-			if (movingRight) xVelocity = maxSpeed;
-			else xVelocity = -maxSpeed;
-		}
-		else xVelocity = 0.0f;
-		rigidbody2D.velocity = new Vector2 (xVelocity, rigidbody2D.velocity.y);
-		anim.SetFloat ("speed", xVelocity);
-		
-		//if moving left and not facing right, flip
-		if ((rigidbody2D.velocity.x > 0 && !facingRight) || (rigidbody2D.velocity.x < 0 && facingRight)) Flip();
-	}
-
-	void Update() {
-		if (curHealth <= 0) Destroy (this.gameObject);
-	}
-	
-	//function to flip the walking animation
-	void Flip() {
-		facingRight = !facingRight;
-		anim.SetBool ("FacingRight", facingRight);
-		transform.localScale = new Vector2 (-transform.localScale.x, transform.localScale.y);
-	}
-	
 	public void AdjustcurHealth(int adj) {
 		curHealth += adj;
+		if (curHealth <= 0) Destroy (this.gameObject);
 	}
 }
