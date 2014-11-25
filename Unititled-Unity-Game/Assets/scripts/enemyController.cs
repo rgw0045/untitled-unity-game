6e46207c0fd469d6e3556760e5c46f8cb72d097c
Gary Johnson <garyleejohnsonjr@gmail.com>
2014-11-25 11:39:19 -0600

    Enemies only shoot at the player if they have line of sight; added more enemies to level 1 - Gary

--- Unititled-Unity-Game/Assets/scripts/enemyController.cs
+++ Unititled-Unity-Game/Assets/scripts/enemyController.cs
@@ -36,8 +36,11 @@ public class enemyController : MonoBehaviour {
 		// get the player's position relative to this enemy's position
 		Vector2 playerRelPos = Vector2.Scale(player.position - enemy.position, enemy.localScale);
 		
+		// check if veiw of the player is blocked
+		bool viewBlocked = Physics2D.Linecast(enemy.position, player.position, LayerMask.GetMask("Walls") + 1).transform;
+		
 		// if the player is in front and is in range attack the player
-		if (playerRelPos.x > 0.0f && playerRelPos.magnitude < attackRange) attack(playerRelPos);
+		if (playerRelPos.x > 0.0f && playerRelPos.magnitude < attackRange && !viewBlocked) attack(playerRelPos);
 		else patrol();
 		
 		// set animation states
