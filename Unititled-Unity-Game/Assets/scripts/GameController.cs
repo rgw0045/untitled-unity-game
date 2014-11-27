6459b6d56c3119b238d80e84502a092aa00d7add
Ricky <ricky@Rickys-MBP.PK5001Z>
2014-11-27 10:40:20 -0600

    Timer Fixed

--- Unititled-Unity-Game/Assets/scripts/GameController.cs
+++ Unititled-Unity-Game/Assets/scripts/GameController.cs
@@ -5,13 +5,24 @@ public class GameController : MonoBehaviour {
 
 	public GameObject player;
 	public GUIText scoreText;
+	public GUIText timeText;
 	private int score;
+	private float startTime;
+	private float stageTime;
 	
 	void Start(){
 		
 		//DontDestroyOnLoad (player);
 		score = 0;
+		startTime = Time.time;
 		UpdateScore ();
+		UpdateTime ();
+	}
+
+	void Update(){
+
+		stageTime = Time.time - stageTime;
+		UpdateTime ();
 	}
 	
 	public void Addscore(int modifier){
@@ -23,5 +34,10 @@ public class GameController : MonoBehaviour {
 	void UpdateScore(){
 		
 		scoreText.text = "Score: " + score;
+	}
+
+	void UpdateTime(){
+
+		timeText.text = "Time: " + stageTime;
 	}
 }
