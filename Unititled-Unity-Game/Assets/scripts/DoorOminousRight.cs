ba7f770b7d08f3d7e645540f689a997d464df08f
Nchard <NicholasChard12@gmail.com>
2014-12-01 18:53:46 -0600

    Added some rooms - No function, simply for easy future expansion

---
+++ Unititled-Unity-Game/Assets/scripts/DoorOminousRight.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorOminousRight : MonoBehaviour {
+
+	private int levelNum;
+
+	void OnTriggerEnter2D(Collider2D other){
+				if (other.tag == "Player") {
+						Application.LoadLevel ("OminousRight");
+				}
+	
+		}
+}
